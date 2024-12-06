9b3cdc156e2184c0852ec858d6b65457d34fe0de
NimrodLoozar <nimrod.lobozarl.com>
2024-12-07 00:14:26 +0100

    update tailwindcss to version 3.4.16 and add new SVG assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,17 +8,29 @@
     <title>Laravel</title>
 
     <!-- Fonts -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    {{-- <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> --}}
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
     <!-- Styles / Scripts -->
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @else
     @endif
+
+    <style>
+        .crop-top {
+            object-fit: cover;
+            object-position: top center;
+            height: 80%;
+            /* Adjust this value to crop more from the top */
+        }
+    </style>
 </head>
 
-<body class="font-sans antialiased dark:bg-black dark:text-white/50">
+<body class="font-sans antialiased">
     <div class="bg-white">
         <header class="absolute inset-x-0 top-0 z-50">
             <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
@@ -30,7 +42,7 @@
                     </a>
                 </div>
                 <div class="flex lg:hidden">
-                    <button type="button"
+                    <button id="mobile-menu-button" type="button"
                         class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
                         <span class="sr-only">Open main menu</span>
                         <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
@@ -41,18 +53,18 @@
                     </button>
                 </div>
                 <div class="hidden lg:flex lg:gap-x-12">
-                    <a href="#" class="text-sm/6 font-semibold text-gray-900">Product</a>
-                    <a href="#" class="text-sm/6 font-semibold text-gray-900">Features</a>
-                    <a href="#" class="text-sm/6 font-semibold text-gray-900">Marketplace</a>
-                    <a href="#" class="text-sm/6 font-semibold text-gray-900">Company</a>
+                    <a href="#about" class="text-sm/6 font-semibold text-white">Over mij</a>
+                    <a href="#" class="text-sm/6 font-semibold text-white">Features</a>
+                    <a href="#" class="text-sm/6 font-semibold text-white">Marketplace</a>
+                    <a href="#" class="text-sm/6 font-semibold text-white">Company</a>
                 </div>
                 <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-                    <a href="#" class="text-sm/6 font-semibold text-gray-900">Log in <span
+                    <a href="#" class="text-sm/6 font-semibold text-white">Log in <span
                             aria-hidden="true">&rarr;</span></a>
                 </div>
             </nav>
             <!-- Mobile menu, show/hide based on menu open state. -->
-            <div class="lg:hidden" role="dialog" aria-modal="true">
+            <div id="mobile-menu" class="lg:hidden hidden" role="dialog" aria-modal="true">
                 <!-- Background backdrop, show/hide based on slide-over state. -->
                 <div class="fixed inset-0 z-50"></div>
                 <div
@@ -64,7 +76,8 @@
                                 src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
                                 alt="">
                         </a>
-                        <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+                        <button id="mobile-menu-close-button" type="button"
+                            class="-m-2.5 rounded-md p-2.5 text-gray-700">
                             <span class="sr-only">Close menu</span>
                             <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                 stroke="currentColor" aria-hidden="true" data-slot="icon">
@@ -75,18 +88,19 @@
                     <div class="mt-6 flow-root">
                         <div class="-my-6 divide-y divide-gray-500/10">
                             <div class="space-y-2 py-6">
+                                <a href="#about"
+                                    class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-gray-50">Over
+                                    mij</a>
                                 <a href="#"
-                                    class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Product</a>
+                                    class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-gray-50">Features</a>
                                 <a href="#"
-                                    class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Features</a>
+                                    class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-gray-50">Marketplace</a>
                                 <a href="#"
-                                    class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Marketplace</a>
-                                <a href="#"
-                                    class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Company</a>
+                                    class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-gray-50">Company</a>
                             </div>
                             <div class="py-6">
                                 <a href="#"
-                                    class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Log
+                                    class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-white hover:bg-gray-50">Log
                                     in</a>
                             </div>
                         </div>
@@ -95,41 +109,149 @@
             </div>
         </header>
 
-        <div class="relative isolate px-6 pt-14 lg:px-8">
-            <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+
+        <div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+            <img src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+                alt="" class="absolute inset-0 -z-10 size-full object-cover object-right md:object-center">
+            <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
                 aria-hidden="true">
-                <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
                     style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
                 </div>
             </div>
-            <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-                <div class="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+                aria-hidden="true">
+                <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+                    style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
+                </div>
+            </div>
+            <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                <div class="relative mx-auto max-w-2xl lg:mx-0">
+                    <h2 class="text-5xl font-semibold tracking-tight text-white sm:text-7xl">Portofolio site van
+                        Ferenc Nimród Lobozár</h2>
+                    <p class="mt-8 text-pretty text-lg font-medium text-gray-300 sm:text-xl/8">Mijn doel is...</p>
 
                 </div>
-                <div class="text-center">
-                    <h1 class="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
-                        Portfoliosite van Ferenc Nimród Lobozár </h1>
-                    <p class="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">Anim aute id magna aliqua
-                        ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
-                        occaecat.</p>
-                    <div class="mt-10 flex items-center justify-center gap-x-6">
-                        <a href="#"
-                            class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get
-                            started</a>
-                        <a href="#" class="text-sm/6 font-semibold text-gray-900">Learn more <span
-                                aria-hidden="true">→</span></a>
+                <div class="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+                    <div
+                        class="grid grid-cols-1 gap-x-8 gap-y-6 text-base/7 font-semibold text-white sm:grid-cols-2 md:flex lg:gap-x-10">
+                        <a href="#">Open roles <span aria-hidden="true">&rarr;</span></a>
+                        <a href="#">Internship program <span aria-hidden="true">&rarr;</span></a>
+                        <a href="#">Our values <span aria-hidden="true">&rarr;</span></a>
+                        <a href="#">Meet our leadership <span aria-hidden="true">&rarr;</span></a>
                     </div>
-                </div>
-            </div>
-            <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-                aria-hidden="true">
-                <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-                    style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
+                    <dl class="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
+                        <div class="flex flex-col-reverse gap-1">
+                            <dt class="text-base/7 text-gray-300">Offices worldwide</dt>
+                            <dd class="text-4xl font-semibold tracking-tight text-white">12</dd>
+                        </div>
+                        <div class="flex flex-col-reverse gap-1">
+                            <dt class="text-base/7 text-gray-300">Full-time colleagues</dt>
+                            <dd class="text-4xl font-semibold tracking-tight text-white">300+</dd>
+                        </div>
+                        <div class="flex flex-col-reverse gap-1">
+                            <dt class="text-base/7 text-gray-300">Hours per week</dt>
+                            <dd class="text-4xl font-semibold tracking-tight text-white">40</dd>
+                        </div>
+                        <div class="flex flex-col-reverse gap-1">
+                            <dt class="text-base/7 text-gray-300">Paid time off</dt>
+                            <dd class="text-4xl font-semibold tracking-tight text-white">Unlimited</dd>
+                        </div>
+                    </dl>
                 </div>
             </div>
         </div>
-    </div>
+        <img src="{{ asset('img/20240807_185820.jpg') }}" alt="Your Name"
+            class="absolute right-40 top-40 w-3/12 scale-110 object-cover z-10 transform crop-top">
 
+        <div id="about" class="bg-white py-24 sm:py-32">
+            <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                <div class="mx-auto max-w-2xl lg:mx-0">
+                    <h2 class="text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Over mij
+                    </h2>
+                    <p class="mt-2 text-lg/8 text-gray-600">Ik ben in <strong>2006</strong> in
+                        <strong>Hongarije</strong> geboren, na mijn tiende zijn we
+                        naar nederland verhuizd, sinds dien leef ik hier. In de vakanties ga bijna elke keer naar
+                        Hongarije toe om
+                        familie te ontmoeten. In 2023 heb ik mijn middelbare afgerond op <strong>mavo</strong> niveau in
+                        Soest op het
+                        <strong class="text-green-500">Griftland College</strong>. Daarna ben ik op
+                        <strong class="text-blue-500">MBO Utrecht ICT Academie</strong> gaan studeren voor
+                        <strong>Software developper niveau 4</strong>.
+                    </p>
+                </div>
+                <div
+                    class="mx-auto mt-10 max-w-2xl border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none">
+                    <div class="flex flex-col">
+                        <h3 class="text-2xl font-semibold text-gray-900">Skills</h3>
+                    </div>
+                    <div class="bg-white">
+                        <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+                            <div
+                                class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                                <div
+                                    class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]">
+                                    <img src="{{ asset('img/html-5-logo.svg') }}" alt="HTML 5" class="scale-90">
+                                    <p class="text-center mt-2 font-semibold text-gray-900">HTML 5</p>
+                                </div>
+                                <div
+                                    class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]">
+                                    <img src="{{ asset('img/css-3-logo.svg') }}" alt="CSS 3">
+                                    <p class="text-center mt-2 font-semibold text-gray-900">CSS 3</p>
+                                </div>
+                                <div
+                                    class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]">
+                                    <img src="{{ asset('img/php-svgrepo-com.svg') }}" alt="PHP">
+                                    <p class="text-center mt-2 font-semibold text-gray-900">PHP</p>
+                                </div>
+                                <div
+                                    class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]">
+                                    <img src="{{ asset('img/laravel.svg.png') }}" alt="Laravel" class="scale-90">
+                                    <p class="text-center mt-2 font-semibold text-gray-900">Laravel</p>
+                                </div>
+                                <div
+                                    class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]">
+                                    <img src="{{ asset('img/git-logo.svg') }}" alt="Git" class="scale-75">
+                                    <p class="text-center mt-2 font-semibold text-gray-900">Git</p>
+                                </div>
+                                <div
+                                    class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]">
+                                    <img src="{{ asset('img/js-logo.svg') }}" alt="JavaScript" class="scale-75">
+                                    <p class="text-center mt-2 font-semibold text-gray-900">JavaScript</p>
+                                </div>
+                                <div
+                                    class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]">
+                                    <img src="{{ asset('img/Blade.png') }}" alt="Blade" class="scale-90">
+                                    <p class="text-center mt-2 font-semibold text-gray-900">Blade</p>
+                                </div>
+                                <div
+                                    class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]">
+                                    <img src="{{ asset('img/mysql-logo.svg') }}" alt="MySQL" class="scale-75">
+                                    <p class="text-center mt-2 font-semibold text-gray-900">MySQL</p>
+                                </div>
+                                <div
+                                    class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]">
+                                    <img src="{{ asset('img/mark.svg') }}" alt="MySQL" class="scale-75">
+                                    <p class="text-center mt-2 font-semibold text-gray-900">Tailwind</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script>
+            document.getElementById('mobile-menu-button').addEventListener('click', function() {
+                var menu = document.getElementById('mobile-menu');
+                menu.classList.toggle('hidden');
+            });
+
+            document.getElementById('mobile-menu-close-button').addEventListener('click', function() {
+                var menu = document.getElementById('mobile-menu');
+                menu.classList.add('hidden');
+            });
+        </script>
 </body>
 
 </html>
